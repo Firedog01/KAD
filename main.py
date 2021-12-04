6fbde73091030bb02d6737daf915e73fd084b6bd
Firedog01 <jan.rubacha@onet.pl>
2021-12-04 22:42:15 +0100

    wykresy dla dane1 wygldaja dobrze

--- main.py
+++ main.py
@@ -1,5 +1,37 @@
+import numpy as np
 from pandas import read_csv
+import matplotlib.pyplot as plt
+import src
+
+# Dla każdego modelu przedstaw:
+# ? sposób przygotowania danych do zastosowania prostej regresji liniowej
+# - wyznaczone wartości parametrów
+# - wykres przedstawiający modelowaną funkcję(-) na tle danych punktów(+)
+# - średni błąd kwadratowy dotyczący wartości funkcji w danych punktach
+# - największą wartość odchylenia wartości funkcji od danych punktów
+# - wartość współczynnika R**2
+# - histogram odchyleń wartości funkcji od danych
+# - (*) test hipotezy statystycznej, że błędy mają rozkład normalny (test chi-kwadrat Pearsona lub test Shapiro-Wilka)
+# - komentarz na temat przydatności zastosowania rozważanego modelu
 
 # Wczytywanie
-reader = read_csv("data1.csv", header=None)
-data = reader.values.tolist()
+data1 = src.get_data("data1.csv")
+data2 = src.get_data("data2.csv")
+data3 = src.get_data("data3.csv")
+data4 = src.get_data("data4.csv")
+
+src.calc_A_and_plot(data1, [
+    (lambda x1: 1),
+    (lambda x1: x1)
+])
+src.calc_A_and_plot(data1, [
+    (lambda x1: x1)
+])
+
+data_x = data1[:, 0]
+data_y = data1[:, 1]
+# print(data_x, data_y)
+print(src.var(data_x), src.cov(data_x, data_x))
+a = src.cov(data_x, data_y) / src.var(data_x)
+print(a)
+
