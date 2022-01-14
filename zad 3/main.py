c23b415a339ce74091fadb1adb665bb01eb18252
Firedog01 <jan.rubacha@onet.pl>
2022-01-14 11:47:22 +0100

    mala poprawka do tworzenia danych i dodanie zrodel

--- zad 3/main.py
+++ zad 3/main.py
@@ -3,7 +3,13 @@ from data_generation import generate_data
 if __name__ == "__main__":
     data_0 = generate_data((0, 0), 2, 200)
 
-    data_1_1 = generate_data((-3, 0), 1, 100)
-    data_1_2 = generate_data((3, 0), 1, 100)
+    data_1 = generate_data((-3, 0), 1, 100)
+    data_1.append(generate_data((3, 0), 1, 100))
 
     print(data_0)
+
+"""
+sources:
+https://www.analyticsvidhya.com/blog/2021/04/k-means-clustering-simplified-in-python/
+https://realpython.com/k-means-clustering-python/
+"""
